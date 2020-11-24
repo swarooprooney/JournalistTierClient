f23dba504f0a3cc03b26f5d159d7a4f6e9c43d20
SWAROOP Shivanna <swarooprooney@gmail.com>
2020-11-24 10:12:01 +0530

    Adding HTTPS support and bootstrap & font awesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  journalists: any;
+  title = 'The Journalist Rating App';
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.getJournalists();
+  }
+
+  getJournalists(): void {
+    this.http
+      .get('https://journalisttier.azurewebsites.net/api/journalist/')
+      .subscribe(
+        (response) => {
+          this.journalists = response;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
 }
